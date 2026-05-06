b9212fdc2343cf3f3cba501e7dcc1aee25d38b0e
aotario <nanda10vm@gmail.com>
2026-05-06 11:53:02 -0300

    adicionando 2 a 3 ao código

--- 1.1-numeros-em-python.py
+++ 1.1-numeros-em-python.py
@@ -30,3 +30,32 @@ print ("Valor:", numero_inteiro)
 print("Tipo:", type(numero_inteiro))
 
 print("-------------------------------")
+
+#EXEMPLO 02 - NUMERO DECIMAL
+
+#Float é um número com ponto decimal
+numero_decimal - 3.14
+
+print ("Valor:", numero_decimal)
+print ("Tipo:", type(numero_decimal))
+
+print("-------------------------------")
+
+#EXEMPLO 03 - NUMEROS COMPLEXOS
+#Um número complexo possui duas partes:
+#Parte real (Número normal)
+#Parte imaginária (Multiplicar por j)
+
+#Estrutura Geral:
+#numero  = a + bj
+
+#a = parte real
+#b = parte imaginária
+#j = unidade imaginária
+
+numero_complexo = 2 + 3j
+
+print(Valor:", numero_complexo")
+print("Tipo", type(numero_complexo))
+
+print("------------------------------")
