506a46c392fec5326765ac8b10cafa9c0175ab4c
aotario <nanda10vm@gmail.com>
2026-05-20 14:28:20 -0300

    Operações aritiméticas3

--- Operações.Aritméticas.py
+++ Operações.Aritméticas.py
@@ -15,41 +15,53 @@ print(type(numero))
 # EX3
 # Converta a string "-3.14" para floatplp
 # e multiplique o resultado por 2.
-string = float("-3.14")
-resultado = string * 2
+num = float("-3.14")
+resultado = num * 2
 print(resultado)
 
 # EX4
 # Tente converter a string "cento e vinte"
 # para inteiro e observe o que acontece.
+valor1 =  "cento e vinte"
+print(valor1)
+print(type(valor1))
 
 
 # EX5
 # Converta o número 42 para string
 # e concatene com a palavra " respostas".
-
+numero1 = str(42)
+print(numero1, "respostas.")
 
 # EX6
 # Use a função complex() para criar
 # um número complexo com parte real 3
 # e parte imaginária 5.
+numerocomplexo = 3 + 5j
+print(numerocomplexo.real)
 
 # EX7
 # Converta o número 0 para booleano
 # e mostre o resultado.
+print(bool(0)) 
 
 # EX8
 # Converta o número -100 para booleano
 # e mostre o resultado.
+print(bool(-100))
 
 # EX9
 # Converta o número 3.1415 para inteiro
 # e depois para string, tudo em uma única linha.
+numero3 = str(int(3.1415))
+print(numero3)
 
 # EX10
 # Some um número inteiro (5) com um float (2.3)
 # e verifique qual é o tipo do resultado.
-
+numero_inteiro = 5
+resultado1 = numero_inteiro + 3,14
+print(type(resultado))
 
 
 
