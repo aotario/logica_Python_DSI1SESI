5423939f3d59e13311a80000542e720df7ea5f9b
aotario <nanda10vm@gmail.com>
2026-05-20 14:06:30 -0300

    exercicio operacoes aritiméticas

---
+++ Operações.Aritméticas.py
@@ -0,0 +1,55 @@
+# EX1
+# O usuário digitou "25" como sua idade em um formulário.
+# Converta para inteiro e calcule a idade que ele terá
+# daqui a 5 anos.
+idade = int(25)
+idade_futura = idade + 5
+print("idade futura: ", idade_futura)
+
+# EX2
+# Converta o número de ponto flutuante 7.999
+# para inteiro e observe o resultado.
+numero = int(7.999)
+print(type(numero))
+
+# EX3
+# Converta a string "-3.14" para floatplp
+# e multiplique o resultado por 2.
+string = float("-3.14")
+resultado = string * 2
+print(resultado)
+
+# EX4
+# Tente converter a string "cento e vinte"
+# para inteiro e observe o que acontece.
+
+
+# EX5
+# Converta o número 42 para string
+# e concatene com a palavra " respostas".
+
+
+# EX6
+# Use a função complex() para criar
+# um número complexo com parte real 3
+# e parte imaginária 5.
+
+# EX7
+# Converta o número 0 para booleano
+# e mostre o resultado.
+
+# EX8
+# Converta o número -100 para booleano
+# e mostre o resultado.
+
+# EX9
+# Converta o número 3.1415 para inteiro
+# e depois para string, tudo em uma única linha.
+
+# EX10
+# Some um número inteiro (5) com um float (2.3)
+# e verifique qual é o tipo do resultado.
+
+
+
+
